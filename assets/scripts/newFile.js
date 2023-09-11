824ac292289c1d6f1266da677c4a5f76f538468f
Terhi Repo <t0rete00@students.oamk.fi>
2023-09-11 11:26:39 +0300

    Trying new things

---
+++ assets/scripts/newFile.js
@@ -0,0 +1,7 @@
+document.getElementById('myForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert('Form submitted!');
+},
+    window.addEventListener('load', function () {
+        console.log('Page fully loaded');
+    };
---
+++ assets/scripts/script.js
@@ -0,0 +1,12 @@
+document.getElementById('btn').onclick = function() {
+    this.style.backgroundColor = 'white';
+};
+document.getElementById('toggleClassButton').ondbclick = function() {
+    this.classList.toggle('active');
+};
+document.getElementById('hoverButton').onclick = function() {
+    this.classList.toggle('active');
+};
+document.getElementById('toggleClassButton').onclick = function() {
+    this.classList.toggle('active');
+};
